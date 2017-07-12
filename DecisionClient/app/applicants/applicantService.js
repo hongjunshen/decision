0d248a5da64b77707e0810801b7f49eb56d37c60
Hongjun Shen <Hongjun Shen>
2017-07-12 06:54:45 -0500

    add service

---
+++ DecisionClient/app/applicants/applicantService.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    angular
+        .module('app')
+        .factory('applicantService', applicantService);
+
+    function applicantService($http) {
+        // config
+        var baseAddress = "http://localhost:64239/api/";
+
+        // public api
+        var service = {
+            getApplicants: getApplicants,
+            save: save
+        };
+
+        return service;
+
+        //retrieve all applicants
+        function getApplicants() {
+            return $http.get(baseAddress + 'applicant/')
+                .then(function (response) {
+                    console.log(response.data);
+                    return response.data;
+                });
+        }
+        
+        //save one applicant
+        function save(applicant) {
+            return $http.post(baseAddress + 'applicant/', applicant).then(function (response) {
+                return response.data;
+            }, function (data) {
+                return  data.ExceptionMessage;
+            });
+        }
+    }
+})();
