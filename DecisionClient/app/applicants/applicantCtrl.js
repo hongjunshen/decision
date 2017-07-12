bc446ed6f89816f71bfde2bbfb381b1217f2916d
Hongjun Shen <Hongjun Shen>
2017-07-12 07:17:49 -0500

    clean up controller

--- DecisionClient/app/applicants/applicantCtrl.js
+++ DecisionClient/app/applicants/applicantCtrl.js
@@ -1,51 +1,6 @@
 ﻿//(function () {
 //    'use strict';
 
-//    angular
-//        .module('app')
-//        .controller('ApplicantCtrl', ApplicantCtrl);
-
-//    function ApplicantCtrl($http) {
-//        var vm = this;
-
-//        vm.currentDate = new Date();
-//        vm.title = 'hi';
-      
-//        vm.getApplicants = function () {
-//            $http.get('http://localhost:64239/api/applicant/')
-//                .then(function (response) {
-//                    vm.applicants = response.data;
-//                    console.log(vm.applicants);
-//                });
-//        }
-
-//        vm.getApplicants();
-
-//        vm.save = function () {
-//            var obj = {
-//                firstName: vm.firstName,
-//                lastName: vm.lastName,
-//                dob: vm.dob,
-//                gpa: vm.gpa
-//            };
-//            $http.post('http://localhost:64239/api/applicant/', obj).then(function (response) {
-//                vm.getApplicants();
-//                vm.reset();
-//            }, function (data) {
-//                vm.error = "An error has occured while adding applicants! " + data.ExceptionMessage;
-//            });
-//        }
-
-//        vm.reset = function () {
-//            vm.firstName = '';
-//            vm.lastName = '';
-//            vm.dob = null;
-//            vm.gpa = null;
-//        }
-//    }
-//})();
-
-
 (function () {
     'use strict';
 
@@ -56,8 +11,10 @@
     function ApplicantCtrl(applicantService) {
         var vm = this;
 
+        //used for setting max value of DOB
         vm.currentDate = new Date();
 
+        //retrieve all applicants
         vm.getApplicants = function () {
             applicantService.getApplicants()
                 .then(function (results) {
@@ -70,6 +27,7 @@
 
         vm.getApplicants();
 
+        //save one applicant to database
         vm.save = function () {
             var obj = {
                 firstName: vm.firstName,
@@ -85,6 +43,7 @@
             });
         }
 
+        //reset form after adding a new record to database
         vm.reset = function () {
             vm.firstName = '';
             vm.lastName = '';
