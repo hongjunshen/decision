6c0301c21e71abfae44e370e7ca0fe47cd319f43
Hongjun Shen <Hongjun Shen>
2017-07-12 07:01:31 -0500

    use service

--- DecisionClient/app/applicants/applicantCtrl.js
+++ DecisionClient/app/applicants/applicantCtrl.js
@@ -1,21 +1,70 @@
-﻿(function () {
+﻿//(function () {
+//    'use strict';
+
+//    angular
+//        .module('app')
+//        .controller('ApplicantCtrl', ApplicantCtrl);
+
+//    function ApplicantCtrl($http) {
+//        var vm = this;
+
+//        vm.currentDate = new Date();
+//        vm.title = 'hi';
+      
+//        vm.getApplicants = function () {
+//            $http.get('http://localhost:64239/api/applicant/')
+//                .then(function (response) {
+//                    vm.applicants = response.data;
+//                    console.log(vm.applicants);
+//                });
+//        }
+
+//        vm.getApplicants();
+
+//        vm.save = function () {
+//            var obj = {
+//                firstName: vm.firstName,
+//                lastName: vm.lastName,
+//                dob: vm.dob,
+//                gpa: vm.gpa
+//            };
+//            $http.post('http://localhost:64239/api/applicant/', obj).then(function (response) {
+//                vm.getApplicants();
+//                vm.reset();
+//            }, function (data) {
+//                vm.error = "An error has occured while adding applicants! " + data.ExceptionMessage;
+//            });
+//        }
+
+//        vm.reset = function () {
+//            vm.firstName = '';
+//            vm.lastName = '';
+//            vm.dob = null;
+//            vm.gpa = null;
+//        }
+//    }
+//})();
+
+
+(function () {
     'use strict';
 
     angular
         .module('app')
         .controller('ApplicantCtrl', ApplicantCtrl);
 
-    function ApplicantCtrl($http) {
+    function ApplicantCtrl(applicantService) {
         var vm = this;
 
         vm.currentDate = new Date();
-        vm.title = 'hi';
-      
+
         vm.getApplicants = function () {
-            $http.get('http://localhost:64239/api/applicant/')
-                .then(function (response) {
-                    vm.applicants = response.data;
-                    console.log(vm.applicants);
+            applicantService.getApplicants()
+                .then(function (results) {
+                    vm.applicants = results;
+                },
+                function (error) {
+                    vm.applicants = null;
                 });
         }
 
@@ -28,11 +77,11 @@
                 dob: vm.dob,
                 gpa: vm.gpa
             };
-            $http.post('http://localhost:64239/api/applicant/', obj).then(function (response) {
+            applicantService.save(obj).then(function (response) {
                 vm.getApplicants();
                 vm.reset();
             }, function (data) {
-                vm.error = "An error has occured while adding applicants! " + data.ExceptionMessage;
+                vm.error = "An error has occured while adding applicants! " + data;
             });
         }
 
@@ -44,3 +93,5 @@
         }
     }
 })();
+
+
